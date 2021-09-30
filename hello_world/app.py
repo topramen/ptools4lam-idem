46eeb80bb2d0fd69e40758e408f4c955671ac60c
topramen2 <ramenon@yahoo.com>
2021-09-30 07:45:13 -0500

    Actually last version did not work, this one does

--- hello_world/app.py
+++ hello_world/app.py
@@ -1,18 +1,30 @@
 from aws_lambda_powertools.utilities.idempotency import (
-    DynamoDBPersistenceLayer, idempotent
+    DynamoDBPersistenceLayer, idempotent, IdempotencyConfig
 )
+import boto3
+import os
 
-persistence_layer = DynamoDBPersistenceLayer(table_name="IdempotencyTable")
+idem_config=IdempotencyConfig(event_key_jmespath="body")
+jls_extract_var = os.environ['IDEMPOTENT_TABLE']
+print (jls_extract_var)
+persistence_layer = DynamoDBPersistenceLayer(table_name=jls_extract_var)
 
+def test_insert():
+    ddb = boto3.resource('dynamodb')
+    table = ddb.Table(os.environ['IDEMPOTENT_TABLE'])
+    table.put_item (Item={'id':'123456789','record':'blahblah'})
+
+
+@idempotent(config=idem_config, persistence_store=persistence_layer)
 @idempotent(persistence_store=persistence_layer)
-def handler(event, context):
-    payment = create_subscription_payment(
-        user=event['user'],
-        product=event['product_id']
-    )
-    ...
+def lambda_handler(event, context):
+    # payment = create_subscription_payment(
+    #     user=event['user'],
+    #     product=event['product_id']
+    # )
+    # test_insert()
+
     return {
-        "payment_id": payment.id,
         "message": "success",
         "statusCode": 200,
     }
